15930a17ae8e0cc6ff30150e25c10623cbdbc261
Ryan-W-Price <51332171+Ryan-W-Price@users.noreply.github.com>
2022-03-06 20:49:48 -0500

    Add files via upload

---
+++ HumansVGoblins/src/Test/com/company/boardObjTest.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class boardObjTest {
+
+    @Test
+    void getX() {
+
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void setX() {
+    }
+
+    @Test
+    void setY() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ HumansVGoblins/src/com/company/Land.java
@@ -0,0 +1,9 @@
+package com.company;
+
+
+public class Land extends boardObj {
+
+    public Land(int x, int y) {
+        super(x, y, "\uD83C\uDF32");
+    }
+}
